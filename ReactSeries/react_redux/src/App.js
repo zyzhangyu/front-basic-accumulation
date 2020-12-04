5870dddeb2021f38ae772849831422df2e557e4d
张宇1 <zylike@Hotmail.com>
2020-12-04 09:40:16 +0800

    1.基础的在react中使用redux

---
+++ ReactSeries/react_redux/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import Home from './pages/home1-手动和redux联系';
+import About from './pages/about';
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+      <About></About>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactSeries/react_redux/src/store/reducer.js
@@ -0,0 +1,29 @@
+import {
+    ADD_NUMBER,
+    SUB_NUMBER,
+    INCREMENT,
+    DECREMENT
+} from './constants.js';
+
+const defaultState = {
+    counter: 0
+}
+
+function reducer(state = defaultState, action) {
+    switch (action.type) {
+        case ADD_NUMBER:{
+            console.log("执行了 加法操作")
+            return {...state, counter: state.counter + action.num };
+        }
+        case SUB_NUMBER:
+            return {...state, counter: state.counter - action.num };
+        case INCREMENT:
+            return {...state, counter: state.counter + 1};
+        case DECREMENT:
+            return {...state, counter: state.counter - 1};
+        default:
+            return state;
+    }
+}
+
+export default reducer;
